2947be921b8c837845149640831ff9ee2bfc4416
Yaroslav Golinskiy <kaiserdem@gmail.com>
2021-03-28 13:47:08 +0300

    First ui + logic

--- ConcentrationApp/ViewController.swift
+++ ConcentrationApp/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    
+    
+    var touches = 0 {
+        didSet {
+            touchLabel.text = "Touches: \(touches)"
+        }
     }
+    
+    
+    func flipButton(emoji: String, button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0, green: 0.4800176024, blue: 1, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
+    let emojiCollection = ["🦊","🐰","🦊","🐰"]
 
-
+    @IBOutlet weak var touchLabel: UILabel!
+    @IBOutlet var buttonCollection: [UIButton]!
+    @IBAction func buttonAction(_ sender: UIButton) {
+        touches += 1
+        if let buttonIndex = buttonCollection.firstIndex(of: sender) {
+            print(buttonIndex)
+            flipButton(emoji: emojiCollection[buttonIndex], button: sender)
+        }
+    }
+    
 }
 
